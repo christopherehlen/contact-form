513de7ee6306cd1503f95a77c8cd043ab2e8f563
Christopher Ehlen <email@christopherehlen.com>
2022-12-30 15:06:43 -0600

    Change defalut to simple.

--- src/preview.js
+++ src/preview.js
@@ -1,11 +1,11 @@
-import { defalut, success, fail, invalid, disabled } from '#src/helpers/preview/states';
+import { simple, success, fail, invalid, disabled } from '#src/helpers/preview/states';
 
 export default {
     adaptors: {},
     filter: {},
     params: {},
     state: {
-        default: defalut,
+        default: simple,
         success: success,
         fail: fail,
         invalid: invalid,
